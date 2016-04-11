6b2d73843c5a197162b6f7d06594fac7950f4aff
Tyler Jones <tylerjones365@gmail.com>
2016-04-10 23:11:05 -0700

    last tweak to config file. NOW Image Rating should be complete

--- hw3/src/configs/Config.php
+++ hw3/src/configs/Config.php
@@ -9,9 +9,8 @@
 */
 
 define ('DB', 'imageRating');
-define ('USER', 'lord_tyler');//ini_get("mysqli.default_user"));
-define ('PWD', '');//ini_get("mysqli.default_password"));
-define ('HOST', 'localhost');//ini_get("mysqli.default_host"));
+define ('USER', ini_get("mysqli.default_user"));
+define ('PWD', ini_get("mysqli.default_password"));
+define ('HOST', ini_get("mysqli.default_host"));
 define ('PORT', ini_get("mysqli.default_port"));
-
 
